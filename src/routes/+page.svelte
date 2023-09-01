710c91b7e91d0cdd30c5ea3eec470678545f5673
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-01 22:10:21 +0200

    add rest

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Recipe Book</title>
+</svelte:head>
+
+<h1>Home</h1>
