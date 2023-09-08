603e62b8df134d9c297d9a8b7bc3edefabf15910
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-08 20:45:40 +0100

    edit models

--- src/lib/server/models.ts
+++ src/lib/server/models.ts
@@ -1,11 +1,23 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const User_Schema = new mongoose.Schema({
-  user: {
-    email: { type: String, require: true, unique: true },
-    password: { type: String, require: true },
-    name: { type: String, require: true },
-  },
+	user: {
+		email: { type: String, require: true, unique: true },
+		password: { type: String, require: true },
+		name: { type: String, require: true }
+	},
+	recipe: [
+		{
+			title: { type: String },
+			description: { type: String },
+			createdAt: { type: String },
+			publishedAt: { type: String },
+			updatedAt: { type: String },
+			public: { type: Boolean },
+			tags: { type: Object },
+			id: { type: String }
+		}
+	]
 });
 
-export const User_Model = mongoose.model("User", User_Schema);
+export const User_Model = mongoose.model('User', User_Schema);
